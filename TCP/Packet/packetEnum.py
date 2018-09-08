e0ec9100a10ee299c0719dc079fb1c6845ef8046
Galaxy1036 <simon.hamelin6500@gmail.com>
2018-09-08 13:16:54 +0200

    CR 2.4 key

--- TCP/Packet/packetEnum.py
+++ TCP/Packet/packetEnum.py
@@ -2,9 +2,17 @@
 
 
 packet_enum = {
+                # Client Packet
                 10100: "ClientHello",
                 10101: "Login",
+                14888: "ClientCapabilities",
+                15665: "KeepAlive",
+
+                # Server Packet
                 20100: "ServerHello",
                 20103: "LoginFailed",
-                24662: "LoginOK"
-                }
+                24662: "LoginOK",
+                20247: "KeepAliveOk",
+                25612: "SectorState",
+                27691: "GoogleAccountAlreadyBound"
+               }
